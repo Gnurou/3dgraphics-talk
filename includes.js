f55d94a56a256557c15ed8f1b29a398388f77719
Alexandre Courbot <acourbot@nvidia.com>
2015-06-17 19:29:42 +0900

    Improve online version

--- includes.js
+++ includes.js
@@ -1,15 +1,29 @@
 var libSources = {
 'reveal' : [ 'lib/reveal.js/', 'https://rawgit.com/hakimel/reveal.js/master/' ],
+'three' : [ 'lib/three.js/', 'https://rawgit.com/mrdoob/three.js/master/' ],
+'threex.windowresize' : [ 'lib/threex.windowresize/', 'https://rawgit.com/jeromeetienne/threex.windowresize/master/' ],
+'tween' : [ 'lib/tween.js/', 'https://rawgit.com/tweenjs/tween.js/master/' ],
+'raphael' : [ 'lib/raphael/', 'https://rawgit.com/DmitryBaranovskiy/raphael/master/' ],
 };
 
-function getLibFile(lib, file)
+function getLibFile(lib, file, path)
 {
-	return libSources[lib][0] + file;
+	var idx;
+
+	if (window.location.protocol === 'file:') {
+		idx = 0;
+	} else {
+		idx = 1;
+		path = null;
+	}
+
+	var r = (path || '') + libSources[lib][idx] + file;
+	return r;
 }
 
-function addScript(lib, script)
+function addScript(lib, script, path)
 {
-	document.write('<script type="text/javascript" src="' + getLibFile(lib, script) + '"></' + 'script>');
+	document.write('<script type="text/javascript" src="' + getLibFile(lib, script, path) + '"></' + 'script>');
 }
 
 function addCSS(lib, css, extra)
